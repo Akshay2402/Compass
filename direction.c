e6e1da154573a58e2e891ea77921b2416e050c82
Akshay Sharma <akshay.sharma2402@gmail.com>
2017-03-07 23:48:57 +0530

    First commit
    please read the read me.txt

---
+++ direction.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define PI 3.14159;
+
+int main(){
+	FILE *fx, *fy;
+	float x;
+	float y;
+	char *dir;
+	dir = malloc(sizeof(char)*15);
+	int heading;
+	int index = 1;
+	
+	fx = fopen("c:\\Users\\Akshay Sharma\\Desktop\\replica\\delcolx.txt","r");
+	if(fx == NULL){
+		printf("File X cannot be opened");
+		exit(1);
+	}
+	
+	fy = fopen("c:\\Users\\Akshay Sharma\\Desktop\\replica\\delcoly.txt","r");
+	if(fy == NULL){
+		printf("File Y cannot be opened");
+		exit(2);
+	}	
+		printf("id\t\tx\t\ty\t  Heading\t\tDirection\n\n");
+	while(fscanf(fx,"%f",&x) != EOF && fscanf(fy,"%f",&y) != EOF ){
+				
+			heading = (atan2(y,x) * 180) / PI;
+			
+			if(heading < 0) 
+				heading += 360;
+			
+			if(heading > 68 && heading >113)  dir = "East";
+			if(heading > 248 && heading <293) dir = "West";
+			if(heading > 338 && heading <23) dir = "North";
+			if(heading > 158 && heading <203) dir = "South";
+			if(heading > 23 && heading < 68 ) dir = "North-East";
+			if(heading > 112 && heading < 158 ) dir = "South-East";
+			if(heading > 203 && heading < 248 ) dir = "South-West";
+			if(heading > 293 && heading < 338 ) dir = "North-West";			
+			
+			printf("%d\tx = %f\ty =%f\tHeading = %d\tDirection = %s\n",index++, x, y,heading,dir);
+			
+	}	
+	
+	fclose(fx);
+	fclose(fy);
+}
